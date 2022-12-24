50f3c079f8ce3faa659e2f376210dc2e925a7d58
alper1616 <alper_pacaci@hotmail.com>
2022-12-24 16:17:29 -0500

    new x and y added

--- src/x.java
+++ src/x.java
@@ -1,6 +1,5 @@
 public class x {
     public static void main(String[] args) {
-        System.out.println("practice addd someting 12
-                           ");
+        System.out.println("practice addd someting 123 ");
     }
 }
